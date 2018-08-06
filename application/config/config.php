854b420ebb98092d6aeb2552cd5b994848681157
Tony Gustafsson <mail@tonyg.se>
2018-08-06 19:39:06 +0200

    Updating codeigniter

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:666/sarabot';
+$config['base_url'] = 'http://mydev.local:82/sarabot/';
 
 /*
 |--------------------------------------------------------------------------
